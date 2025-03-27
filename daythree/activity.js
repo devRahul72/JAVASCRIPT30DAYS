8d1ff36d7c96b7700ac74bce0333488a189892e5
Rahul kumar <rahlkumar9504599465gmail.com>
2025-03-27 18:28:14 +0530

    activity 1

---
+++ daythree/activity.js
@@ -0,0 +1,13 @@
+
+//Write a program to check the sign of a number using if else if else statement
+let a=-1;
+
+if(a>0){
+    console.log("The sign is +");
+}
+else if(a<0){
+    console.log("The sign is -");
+}
+else{
+    console.log("The sign is 0");
+}
